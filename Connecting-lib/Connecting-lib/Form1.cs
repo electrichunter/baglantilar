e8532361f2d8403871c71d2be05f08e3aa9251d0
Ömer Faruk Uysal <ouysal155@gmail.com>
2024-04-29 17:43:36 +0300

    Sql veri ekleme işlemi
    Sadece class kulllanmaya çalışarak sql veri ekleme

---
+++ Connecting-lib/Connecting-lib/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Connecting_lib
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+        sql_data sql_Data = new sql_data();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sql_Data.Sqlbaglan();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string isim = txtIsim.Text;
+                string soyisim = txtSoyisim.Text;
+                string telefon = txtTelefon.Text;
+        
+                string departman = txtDepartman.Text;
+                string sehir = txtSehir.Text;
+                string dogumTarihi = txtDogumTarihi.Text;
+                string maas = txtMaas.Text; 
+                string mail = txtMail.Text;
+
+                sql_Data.SqlEkle(isim, soyisim, telefon,departman, sehir, dogumTarihi, maas, mail);
+
+                MessageBox.Show("Çalışan başarıyla eklendi!", "Başarılı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Bir hata oluştu: {ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+    }
+
---
+++ Connecting-lib/Connecting-lib/sql-data.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Connecting_lib
+{
+    internal class sql_data
+    {
+       internal string Sqlbaglan()
+        {
+            // Doğru veritabanı adı ile bağlantı dizisini ayarlayın
+            string connectionString = "Data Source=(local);Initial Catalog=xyzbank;Integrated Security=True";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                return "Bağlantı başarıyla açıldı!";
+            }
+        }
+
+        public void SqlEkle(
+            string isim, 
+            string soyisim, 
+            string telefon, 
+           
+            string departman, 
+            string sehir, 
+            string dogumTarihi, 
+            string maas, 
+            string mail)
+        {
+            try
+            {
+                string connectionString = "Data Source=(local);Initial Catalog=xyzbank;Integrated Security=True";
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string query = "INSERT INTO Customers ( Isimler, Soyisimler, Telefon,  Departman, Sehir, DogumTarihi, Maas, Mail) VALUES (@isim, @soyisim, @telefon , @departman, @sehir, @dogumTarihi, @maas, @mail)";
+                    int yeniId = 1000;
+                    for (int i = 0; i < 100000000; i++)
+                    {
+                        yeniId++; // ID değerini artır
+                    }
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@id", yeniId); // yeniId, belirlediğiniz ID
+
+                        command.Parameters.AddWithValue("@isim", isim);
+                        command.Parameters.AddWithValue("@soyisim", soyisim);
+                        command.Parameters.AddWithValue("@telefon", telefon);
+                        command.Parameters.AddWithValue("@departman", departman);
+                        command.Parameters.AddWithValue("@sehir", sehir);
+                        command.Parameters.AddWithValue("@dogumTarihi", dogumTarihi);
+                        command.Parameters.AddWithValue("@maas", maas);
+                        command.Parameters.AddWithValue("@mail", mail);
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+
+                Console.WriteLine("Veri başarıyla eklendi!");
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"SQL Error: {ex.Message}");
+            }
+        }
+    }
+}
